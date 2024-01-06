8902b5a941b74fa0621a5cebd0638f90f373067d
Sanchitkanojia <121440897+Sanchitkanojia@users.noreply.github.com>
2024-01-07 01:53:55 +0530

    Update implemented 50_Sql_Queries.sql

--- implemented 50_Sql_Queries.sql
+++ implemented 50_Sql_Queries.sql
@@ -332,6 +332,24 @@ select department, sum(salary) total_sal from worker group by department order b
 -- salary.
 select first_name,salary from worker where salary=(select max(salary) from worker);
 
+## Bonus Question:
+	
+use temp;
+create table pairs(
+A int,
+B int
+);
+
+insert into pairs values (1,2),(2,4),(2,1),(3,2),(4,2),(5,6),(6,5),(7,8);
+select * from pairs;
+
+-- Method 1: Joins
+select lt.* from pairs lt left join pairs rt on lt.A=rt.B and lt.B=rt.A
+where rt.A is null or lt.A < rt.A;
+
+-- Method 2: Corelated subquery
+select * from pairs p1 where not exists
+(select * from pairs p2 where p1.B=p2.A and p1.A=p2.B and p1.A > p2.A);
 
 
 
